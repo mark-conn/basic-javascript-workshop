e945966892a6aa70fc979804dbb972d8c2714ca1
Mark Conn <misterconn@gmail.com>
2016-10-04 17:34:28 +0000

    answer 2

--- answers.js
+++ answers.js
@@ -1,7 +1,15 @@
 // one line comment
 
-//takes a string argument and returns its first character
+//takes a string argument and returns its first characte
 var firstChar = function(string) {
     return string.charAt(0);
     
 }
+
+//takes a string argument and returns last character
+var lastChar = function(string) {
+    return string.charAt(string.length -1);
+    
+}
+lastChar("Mark");
+lastChar("Kitching");
