aec207872ee2d3cb084ea68c7c285aa89bca562b
Mark Conn <misterconn@gmail.com>
2016-10-04 22:55:10 +0000

    answer 14

--- answers.js
+++ answers.js
@@ -194,6 +194,47 @@ function arraySum(array) {
 arraySum([4,5, "string", 4]);
 
 
+//takes two arrays, returns an array with elements that are NOT *shared* between two initial arrays
+function notShared(arr1, arr2) {
+    var newArray = [];
+    for(var i = 0; i < arr1.length; i++) {
+        
+        if(arr2.indexOf(arr1[i]) === -1) {
+            
+            newArray.push(arr1[i]);
+            
+            }
+            
+        }
+        
+    for(var j = 0; j < arr2.length; j++ ) {
+        
+        if(arr1.indexOf(arr2[j]) === -1) {
+            
+            newArray.push(arr2[j]);
+            
+            }
+        
+        } return newArray;
+        
+}
+
+notShared([1,2,3], [3,4,5,6]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
